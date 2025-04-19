540a0d97c9c3e2ffa5207becb5b084664d2702f1
HongBeom <hongbeom7877@naver.com>
2025-04-19 18:01:30 +0900

    Initial Commit

---
+++ LLMPractice/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  LLMPractice
+//
+//  Created by 안홍범 on 4/19/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ LLMPractice/LLMPracticeApp.swift
@@ -0,0 +1,32 @@
+//
+//  LLMPracticeApp.swift
+//  LLMPractice
+//
+//  Created by 안홍범 on 4/19/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LLMPracticeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
